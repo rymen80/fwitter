2c29d864787f8fa32b7234de889ce7d371e1a12e
Ryan Mendez <rjmendez80@gmail.com>
2020-10-12 12:40:45 -0700

    api user routes updated

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const connection = require('../../../config/connection');
 
 // /api/users prepended to every Route
 router.route('/')
   .get(((_req, res) => {
     res.send('Hello');
-  }));
+  }))
+  .post(async (req, res) => {
+    const userInput = req.body;
+    const query = 'INSERT INTO  users SET ?;';
+    const result = await connection.query(query, userInput);
+    res.json(result);
+  });
 
 module.exports = router;
